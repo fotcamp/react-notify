d5f98f78cd93098e6205a0ed0a44aef09cb57a5e
seungdeok <akk9132@naver.com>
2023-12-24 22:16:45 +0900

    chore(@react-popup-craft/snackbar): update gitignore

---
+++ packages/core/react-snackbar/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Snackbar } from './lib/Snackbar';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<Snackbar />);
